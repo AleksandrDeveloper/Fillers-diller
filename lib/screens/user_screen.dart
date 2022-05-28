bba7815e87fd77a740977c489e62003f4cffaac9
AleksandrDeveloper <aleksandr.developer.web@gmail.com>
2022-05-28 15:17:38 +0300

    create user screen statteleswidget

---
+++ lib/screens/user_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatelessWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: UserScreenWidget(),
+    );
+  }
+}
+
+class UserScreenWidget extends StatelessWidget {
+  const UserScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
